4d51e063c384e9fd45226ac6f4f6b503bf93b24a
kashwin007-pixel <k.ashwin007@gmail.com>
2026-02-27 12:11:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Ashwin Kumar Portfolio Loaded");
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.style.opacity = 1;
+            entry.target.style.transform = "translateY(0)";
+        }
+    });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+    section.style.opacity = 0;
+    section.style.transform = "translateY(50px)";
+    section.style.transition = "all 0.8s ease";
+    observer.observe(section);
+});
